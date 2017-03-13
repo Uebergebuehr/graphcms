7aa5b36bc997d1b86600e558e3aa49d922afa8e2
Andreas Schaeffer <andreas.schaeffer@uebergebuehr.de>
2017-03-13 18:12:06 +0100

    Added dependencies

---
+++ src/main/java/org/uebergebuehr/graphcms/controller/SampleController.java
@@ -0,0 +1,27 @@
+package org.uebergebuehr.graphcms.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author Andreas Schaeffer
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleController.class, args);
+	}
+
+}
